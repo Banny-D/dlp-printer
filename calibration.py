5357461603a54cc0fc69a6b7144b8fe32045c9aa
Xinyi <xinyi.bit@qq.com>
2024-01-24 16:20:03 +0800

    240124实现循环绘制点阵

---
+++ calibration.py
@@ -0,0 +1,250 @@
+from PyQt5.QtCore import QObject
+import cv2
+import numpy as np
+import json
+import sys
+from PyQt5.QtWidgets import QWidget
+# from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import time
+
+camera_realtime = False
+
+class PrinterThread(QThread):
+    reset_printer_signal = pyqtSignal(bool)
+    def __init__(self) -> None:
+        super().__init__()
+        self.out_win = 'out_fullscreen' # 默认输出窗口名称
+        cv2.namedWindow(self.out_win, cv2.WND_PROP_FULLSCREEN)
+        # 屏幕设置
+        screens = QApplication.desktop()
+        if screens.screenCount() > 1:
+            # 屏幕位置大小
+            geometry = QApplication.desktop().screenGeometry(1)
+            # 移动屏幕
+            cv2.moveWindow(self.out_win, geometry.x(), geometry.y())
+        else:
+            geometry = QApplication.desktop().screenGeometry()
+        self.out_geo = geometry
+        self.reset_out_img()
+        self.out_img = draw_center_cross(self.out_img)
+        cv2.imshow(self.out_win, self.out_img)
+        cv2.setWindowProperty(self.out_win, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        print('投影设置完成')
+    
+    def reset_out_img(self):
+        self.out_img = np.zeros((self.out_geo.height(), self.out_geo.width(), 3), np.uint8)
+    
+    def update_out_window(self):
+        cv2.imshow(self.out_win, self.out_img)
+        cv2.setWindowProperty(self.out_win, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        self.reset_printer_signal.emit(True)
+    
+    def run(self):
+        # self.reset_out_img()
+        l_unit = int(min(self.out_img.shape[0], self.out_img.shape[1]) * 0.1) # 单位间隔
+        c_point = (self.out_img.shape[1] // 2, self.out_img.shape[0] // 2)
+        for i in range(5):
+            print(f'i = {i}')
+            if i == 0:
+                self.reset_out_img()
+                self.out_img = draw_circle(self.out_img, c_point)
+                self.update_out_window()
+                time.sleep(1)
+                continue
+            for j in range(8):
+                print(f'j = {j}')
+                self.reset_out_img()
+                point = get_point_by_radius(c_point, l_unit*i, j)
+                self.out_img = draw_circle(self.out_img, point)
+                self.update_out_window()
+                time.sleep(1)
+        print('ok')
+
+class CameraCali(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.win_name = 'Camera Calibration' # 默认输出窗口名称
+        self.start_cali = False # 是否开始校准程序
+        self.center_point = False # 是否存在中心点
+        self.init_ui()
+        if camera_realtime:
+            self.init_camera()
+        else:
+            self.brg_frame = cv2.imread('pic.jpg')
+        self.printer_thread = PrinterThread()
+
+    def init_ui(self):
+        self.setWindowTitle('相机校准')
+        # 下拉框
+        self.camera_box = QComboBox(self)
+        # 图像显示区域
+        self.image_area = QLabel(self)
+        # 阈值
+        self.thresh_slider = QSlider(Qt.Horizontal)
+        self.thresh_slider.setRange(0 , 200)
+        self.thresh_slider.setValue(100)
+        # self.thresh_slider.setFixedWidth(500)
+        # 开始校准按钮
+        self.start_cali_button = QPushButton('开始校准', self)
+        # 重新设置阈值
+        self.restart_button = QPushButton('重设阈值', self)
+
+        # 信号与槽连接
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_frame)
+        # self.timer.timeout.connect(self.update_out_window)
+        self.timer.start(30)
+        self.camera_box.currentIndexChanged.connect(self.reset_camera)
+        self.thresh_slider.valueChanged.connect(self.update_frame)
+        self.start_cali_button.clicked.connect(self.cali_func)
+        self.restart_button.clicked.connect(self.restart)
+
+        # 组件布局
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.image_area, alignment = Qt.AlignCenter)
+        layout.addWidget(self.camera_box)
+        layout.addWidget(self.thresh_slider)
+        layout.addWidget(self.start_cali_button)
+        layout.addWidget(self.restart_button)
+
+
+    def init_camera(self):
+        # 读取相机列表
+        print('正在读取相机')
+        for i in range(5):
+            cap = cv2.VideoCapture(i)
+            if not cap.isOpened():
+                break
+            self.camera_box.addItem('相机' + str(i+1))
+            print(f'读取相机{i+1}...')
+            cap.release()
+        self.camera_box.setCurrentIndex(0)
+        self.reset_camera()
+        print('读取完成')
+
+    
+    
+    def reset_camera(self):
+        self.cap = cv2.VideoCapture(self.camera_box.currentIndex())
+        # 读取原本宽高
+        width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        # 等比重置宽高
+        if width > 800:
+            self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
+            self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, int(800/width*height))
+
+    def update_frame(self):
+        self.center_point = None
+        if camera_realtime: # 实时成像
+            ret, frame = self.cap.read()
+        else: # 图片
+            frame = self.brg_frame
+            ret = True
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            _ , frame = cv2.threshold(frame, self.thresh_slider.value(), 255, cv2.THRESH_BINARY)
+            if self.start_cali: # 开始校准，标注点位置
+                self.center_point = find_center(frame)
+                frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+                frame = draw_cross(frame, self.center_point, 10)
+            else:
+                frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+                self.binary = frame # RGB
+            h, w, ch = frame.shape
+            bytes_per_line = ch * w
+            convert_to_Qt_format = QImage(frame.data, w, h, bytes_per_line, QImage.Format_RGB888)
+            p = convert_to_Qt_format.scaled(w, h, Qt.IgnoreAspectRatio)
+            self.image_area.setPixmap(QPixmap.fromImage(p))
+
+    def cali_func(self):
+        # self.timer.stop()
+        self.start_cali = True
+        self.thresh_slider.setDisabled(True)
+        points_cam = []
+        points_scr = []
+        self.printer_thread.start()
+        self.printer_thread.reset_printer_signal.connect(self.print_test)
+        # l_unit = int(min(self.out_img.shape[0], self.out_img.shape[1]) * 0.1) # 单位间隔
+        # c_point = (self.out_img.shape[1] // 2, self.out_img.shape[0] // 2)
+        # for i in range(2):
+        #     print(f'i = {i}')
+        #     if i == 0:
+        #         self.reset_out_img()
+        #         self.out_img = draw_circle(self.out_img, c_point)
+        #         self.update_out_window()
+        #         time.sleep(1)
+        #         continue
+        #     for j in range(8):
+        #         print(f'j = {j}')
+        #         self.reset_out_img()
+        #         point = get_point_by_radius(c_point, l_unit*i, j)
+        #         self.out_img = draw_circle(self.out_img, point)
+        #         self.update_out_window()
+        #         time.sleep(1)
+        # print('ok')
+
+    def restart(self):
+        self.timer.start(30)
+        self.start_cali = False
+        self.thresh_slider.setDisabled(False)
+    
+    def print_test(self, status): #调试用的函数
+        print(status)
+
+def draw_center_cross(img, line_w=2):
+    c_x = img.shape[1] // 2
+    c_y = img.shape[0] // 2
+    # cross_lx = int(img.shape[1] * 0.1)
+    # cross_ly = int(img.shape[0] * 0.1)
+    cross_l = int(min(img.shape[0], img.shape[1]) * 0.1)
+    # img = cv2.line(img, (c_x - cross_lx, c_y), (c_x + cross_lx, c_y), (255, 255, 255), line_w)
+    # img = cv2.line(img, (c_x, c_y - cross_ly), (c_x, c_y + cross_ly), (255, 255, 255), line_w)
+    img = cv2.line(img, (c_x - cross_l, c_y), (c_x + cross_l, c_y), (255, 255, 255), line_w)
+    img = cv2.line(img, (c_x, c_y - cross_l), (c_x, c_y + cross_l), (255, 255, 255), line_w)
+    img = cv2.circle(img, (c_x, c_y), int(0.4 * cross_l), (0, 0, 0), -1)
+    for i in range(-1,2):
+        for j in range(-1,2):
+            img = draw_circle(img, (c_x + i*cross_l, c_y + j*cross_l))
+    return img
+
+def draw_circle(img, center):
+    return cv2.circle(img, center, 3, (255, 255, 255), -1)
+
+def draw_cross(img, center, cr_size):
+    cv2.line(img, (center[0]-cr_size, center[1]), (center[0]+cr_size, center[1]), (255, 0, 0), int(cr_size/5))
+    cv2.line(img, (center[0], center[1]-cr_size), (center[0], center[1]+cr_size), (255, 0, 0), int(cr_size/5))
+    return img
+
+# 轮廓拟合
+def find_center(binary_img):
+    # 边缘检测
+    contours= cv2.findContours(binary_img, cv2.RETR_TREE, 
+                                cv2.CHAIN_APPROX_SIMPLE)[0]
+    cX = None
+    cY = None
+    for i in contours:
+        M = cv2.moments(i)
+        if M["m00"] :
+            x = int(M["m10"] / M["m00"])
+            y = int(M["m01"] / M["m00"])
+            if cX is None or x < cX:
+                cX = x
+                cY = y
+    return [cX, cY]
+
+def get_point_by_radius(p_center, radius, index):
+    # index = [0, 8)
+    angle = index * 0.25*np.pi
+    x = p_center[0] + radius * np.sin(angle)
+    y = p_center[1] - radius * np.cos(angle)
+    return (int(x), int(y))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = CameraCali()
+    window.show()
+    sys.exit(app.exec_())
